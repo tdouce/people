273bbd71059ec5f680b67147ddaac9e3a8e06746
Travis Douce <travisdouce@gmail.com>
2020-10-06 23:08:03 -0400

    Add RecordService#find_records
    This class provides the ability to retrieve records from the database and sort them.

--- app/services/records/record_service.rb
+++ app/services/records/record_service.rb
@@ -7,5 +7,24 @@ module Records
 
       record
     end
+
+    def self.find_records(sort:, database_klass: MemoryDb, record_sorting_service_klass: Records::RecordSortingService)
+      all_records = database_klass.all_records
+
+      record_sorting_service = record_sorting_service_klass.new(records: all_records)
+
+      records = case
+                when sort == 'gender'
+                  record_sorting_service.sort_by_gender
+                when sort == 'birthdate'
+                  record_sorting_service.sort_by_date_of_birth
+                when sort == 'name'
+                  record_sorting_service.sort_by_last_name
+                else
+                  record_sorting_service.sort_by_gender
+                end
+
+      records
+    end
   end
 end
--- spec/services/records/record_service_spec.rb
+++ spec/services/records/record_service_spec.rb
@@ -34,5 +34,70 @@ describe Records::RecordService do
         expect(response).to eq(record)
       end
     end
+
+    describe '.find_records' do
+      let(:database_klass) { class_double(MemoryDb) }
+      let(:record_sorting_service_klass) { class_double(Records::RecordSortingService) }
+      let(:record_sorting_service) { instance_double(Records::RecordSortingService) }
+      let(:sorted_records) { expected_response }
+      let(:expected_response) { 'some_sorted_records' }
+      let(:all_records) { 'some_all_records' }
+
+      subject(:response) do
+        described_class.find_records(
+          sort: sort,
+          database_klass: database_klass,
+          record_sorting_service_klass: record_sorting_service_klass
+        )
+      end
+
+      before do
+        expect(database_klass).to receive(:all_records).and_return(all_records)
+
+        expect(record_sorting_service_klass).to receive(:new).with(
+          records: all_records
+        ).and_return(record_sorting_service)
+      end
+
+      context "when sort is 'gender'" do
+        let(:sort) { 'gender' }
+
+        it 'returns records sorted by gender' do
+          expect(record_sorting_service).to receive(:sort_by_gender).and_return(sorted_records)
+
+          expect(response).to eq(expected_response)
+        end
+      end
+
+      context "when sort is 'birthdate'" do
+        let(:sort) { 'birthdate' }
+
+        it 'returns records sorted by date_of_birth' do
+          expect(record_sorting_service).to receive(:sort_by_date_of_birth).and_return(sorted_records)
+
+          expect(response).to eq(expected_response)
+        end
+      end
+
+      context "when sort is 'name'" do
+        let(:sort) { 'name' }
+
+        it 'returns records sorted by last_name' do
+          expect(record_sorting_service).to receive(:sort_by_last_name).and_return(sorted_records)
+
+          expect(response).to eq(expected_response)
+        end
+      end
+
+      context 'when sort is unrecognized' do
+        let(:sort) { 'unrecognized' }
+
+        it 'returns records sorted by gender' do
+          expect(record_sorting_service).to receive(:sort_by_gender).and_return(sorted_records)
+
+          expect(response).to eq(expected_response)
+        end
+      end
+    end
   end
 end
