05c326d791a5daf4eb0f097fc34a6f802485fdf0
Travis Douce <travisdouce@gmail.com>
2020-10-06 21:28:08 -0400

    Add Record
    This represents the domain object for a 'record'.

---
+++ app/models/record.rb
@@ -0,0 +1,8 @@
+Record = Struct.new(
+  :last_name,
+  :first_name,
+  :gender,
+  :favorite_color,
+  :date_of_birth,
+  keyword_init: true
+)
---
+++ app/models/record_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe Record do
+  describe '#new' do
+    let(:last_name) { 'some_last_name' }
+    let(:first_name) { 'some_first_name' }
+    let(:gender) { 'some_gender' }
+    let(:favorite_color) { 'some_favorite_color' }
+    let(:date_of_birth) { 'some_date_of_birth' }
+    let(:hash) do
+      {
+        last_name: last_name,
+        first_name: first_name,
+        gender: gender,
+        favorite_color: favorite_color,
+        date_of_birth: date_of_birth,
+      }
+    end
+
+    it 'can be initialized with a hash' do
+      response = described_class.new(hash)
+
+      expect(response.first_name).to eq(first_name)
+      expect(response.last_name).to eq(last_name)
+      expect(response.gender).to eq(gender)
+      expect(response.favorite_color).to eq(favorite_color)
+      expect(response.date_of_birth).to eq(date_of_birth)
+    end
+  end
+end
